8fe43621fd7954e22e58240418de7c880c472e07
Raul Palacios <palacios.raul02@gmail.com>
2020-06-17 20:53:04 -0500

    change json format

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,13 +31,13 @@ module.exports = {
       resolve: `gatsby-source-firestore`,
       options: {
         appConfig: {
-          "apiKey": process.env.GATSBY_API_KEY,
-          "authDomain": process.env.GATSBY_AUTH_DOMAIN,
-          "databaseURL": process.env.GATSBY_DB_URL,
-          "projectId": process.env.GATSBY_PROJECT_ID,
-          "storageBucket": process.env.GATSBY_STORAGE_BUCKET,
-          "messagingSenderId": process.env.GATSBY_MESSAGING_SENDER,
-          "appId": process.env.GATSBY_APP_ID
+          apiKey: process.env.GATSBY_API_KEY,
+          authDomain: process.env.GATSBY_AUTH_DOMAIN,
+          databaseURL: process.env.GATSBY_DB_URL,
+          projectId: process.env.GATSBY_PROJECT_ID,
+          storageBucket: process.env.GATSBY_STORAGE_BUCKET,
+          messagingSenderId: process.env.GATSBY_MESSAGING_SENDER,
+          appId: process.env.GATSBY_APP_ID
         },
         types: [{
           type: `Project`,
